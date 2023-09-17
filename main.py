73373fd9143172d6503f54324c9bf0622d6b91f5
shmuelSigler <yakovsig@gmail.com>
2023-09-17 15:38:24 +0300

    replace downtime.txt to downtime.json

--- main.py
+++ main.py
@@ -4,66 +4,119 @@ import json
 import random
 import os
 
-# URL to monitor
+
 url = "https://chargeflow.io"
-instatus_api_key = "bf6b967c74e3067ecbf47e8c9a713cda"
-
-
-def update_instatus(status):
-    headers = {
-        "Authorization": f"Bearer {instatus_api_key}",
+pagerduty_api_token = "e+9dgFyQBf-kzPmaSB3A"
+pagerduty_api_url = "https://api.pagerduty.com/incidents"
+headers = {
+        "Authorization": f"Token token={pagerduty_api_token}",
+        "Accept": "application/vnd.pagerduty+json;version=2",
         "Content-Type": "application/json",
+        "From": "daniela@eatbright.co"
     }
+
+
+def open_incident():
 
     data = {
-        "status": status,
+        "incident": {
+            "type": "incident",
+            "title": "chargeflow.io website is down",
+            "service": {
+                "id": "P5A827W",    # From querystring in URL https://eatbright.eu.pagerduty.com/service-directory/P5A827W
+                "type": "service_reference"
+            }
+        }
     }
 
-    response = requests.put(
-        f"https://api.instatus.com/v2/clmk8mhlx51086bbmxegve6cb3",
+    response = requests.post(
+        pagerduty_api_url,
         headers=headers,
         data=json.dumps(data),
     )
 
-    # print(response.json()["status"])
+    if response.status_code == 201:
+        print("Incident opened successfully.")
+        incident_id = response.json()["incident"]["id"]
+        return incident_id
+    else:
+        print("Failed to open incident.")
+        print(response.text)  # Print the error message, if any
+        return None
+
+
+def close_incident(incident_id):
+
+    data = {
+        "incident": {
+            "type": "incident_reference",
+            "status": "resolved"
+        }
+    }
+
+    response = requests.put(
+        f"{pagerduty_api_url}/{incident_id}",
+        headers=headers,
+        data=json.dumps(data),
+    )
+
+    if response.status_code == 200:
+        print("Incident closed successfully.")
+    else:
+        print("Failed to close incident.")
+        print(response.text)  # Print the error message, if any
 
 
 def monitor_website():
     try:
         response = requests.get(url)
-        response.raise_for_status()     # Raise an exception for HTTP errors
-        return "UP"
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        return True
     except requests.exceptions.RequestException:
-        return "HASISSUES"
+        return False
+
+
+def read_downtime():
+    with open("downtime.json", "r") as json_file:
+        data = json.load(json_file)
+        return data.get("downtime"), data.get("incident_id")
+
+
+def write_downtime(downtime, incident_id):
+    data = {
+        "downtime": downtime,
+        "incident_id": incident_id,
+    }
+    with open("downtime.json", "w") as json_file:
+        json.dump(data, json_file)
 
 
 def main():
-    current_hour = time.localtime().tm_hour     # The hour (0-23)
-    current_min = time.localtime().tm_min       # The minute (0-59)
+    current_hour = time.localtime().tm_hour
+    current_min = time.localtime().tm_min
     downtime = 0
+    previous_downtime, previous_incident_id = read_downtime()
+
     print("hour and minute:", current_hour, current_min)
-    with open("downtime.txt", "r") as file:
-        previous_downtime = int(file.read())
+    print("previous_downtime:", previous_downtime, "previous_incident_id:", previous_incident_id)
 
-    print("previous_downtime:", previous_downtime)
-
-    # Simulate failures at specific times (2:00am, 4:00am, 6:00am)
     if previous_downtime < 10:
-        if 0 <= current_min <= 10:
+        # Simulate failures at specific times (2:00am, 4:00am, 6:00am)
+        if current_hour % 2 == 0 and current_min == 0:
             downtime = random.randint(5, 15)
-            print(downtime)
-            status = "HASISSUES"
+            is_up = False
         else:
-            status = monitor_website()
+            is_up = monitor_website()
+            if previous_incident_id and is_up:
+                close_incident(previous_incident_id)
+        incident_id = None if is_up else open_incident()
     else:
-        status = "HASISSUES"
+        incident_id = previous_incident_id
 
-    # Save the current downtime value for the next run
-    with open("downtime.txt", "w") as downtime_file:
-        downtime_file.write(str(downtime))
+    # Save the current downtime value and incident ID for the next run
+    write_downtime(downtime, incident_id)
 
     print("new downtime:", downtime)
-    update_instatus(status)
 
 
 if __name__ == "__main__":
