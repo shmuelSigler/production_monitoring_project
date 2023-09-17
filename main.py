11bdd4529ae54d3cfb0faa1cb32452ec1c43f002
shmuelSigler <yakovsig@gmail.com>
2023-09-17 10:47:02 +0300

    //

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
 
     # Simulate failures at specific times (2:00am, 4:00am, 6:00am)
     if previous_downtime < 10:
-        if True:
+        if 0 <= current_min <= 10:
             downtime = random.randint(5, 15)
             print(downtime)
             status = "HASISSUES"
